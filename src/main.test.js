dbcfc7eb035ed8747bf6a3220ab7a45986e9fdb7
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-10-10 18:41:34 -0700

    add test for different functions
    added test to capitalized, reverseString, and
    calculator object with functions add, subtract, multiply, divide

---
+++ src/main.test.js
@@ -0,0 +1,25 @@
+const main = require("./main");
+
+test("capitalize me to ME", () => {
+  expect(main.capitalized("me")).toBe("ME");
+});
+
+test("reverse the string Justice to icetsuJ", () => {
+  expect(main.reverseString("Justice")).toBe("icetsuJ");
+});
+
+test("add 7 + 6 is 13", () => {
+  expect(main.calculator.add(7, 6)).toBe(13);
+});
+
+test("subtract 7 + 6 is 1", () => {
+  expect(main.calculator.subtract(7, 6)).toBe(1);
+});
+
+test("multiply 7 * 6 is 42", () => {
+  expect(main.calculator.multiply(7, 6)).toBe(42);
+});
+
+test("divide 7 / 7 is 1", () => {
+  expect(main.calculator.divide(7, 7)).toBe(1);
+});
